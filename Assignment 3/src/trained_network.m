b365bae6713c5e75c5829f77b81f11b19fbb856c
metalhead_6666 <noe.godinho.92@gmail.com>
2015-11-14 03:23:47 +0000

    things

--- Assignment 3/src/trained_network.m
+++ Assignment 3/src/trained_network.m
@@ -1,3 +1,8 @@
 function handles = trained_network(handles)
-    
+    if exist('network.mat', 'file')
+        load('network.mat');
+        
+    else
+        handles = train_network(handles);
+    end
 end
