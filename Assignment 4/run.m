be7882624c3fc5a97d5a3cc2c3c5a0fb6ef9f54d
metalhead_6666 <noe.godinho.92@gmail.com>
2015-12-14 13:08:11 +0000

    made some corrections

--- Assignment 4/run.m
+++ Assignment 4/run.m
@@ -3,7 +3,6 @@
 reference_function = input_function('Select the reference function:\n1-Square\n2-Sin\n', [1,2]);
 controller_type = input_function('Select the controller:\n1-Mamdani\n2-Sugeno\n', [1,2]);
 rules = input_function('Select the number of rules:\n1-9 rules\n2-25 rules\n', [1,2]);
-membership_function = input_function('Select the membership function:\n1-Gaussmf\n2-Trimf\n', [1,2]);
 
 if controller_type == 1
     defuzzification_method = input_function('Select the defuzzification method:\n1-Centroid\n2-Mom\n', [1,2]);
@@ -11,7 +10,7 @@ else
     defuzzification_method = input_function('Select the defuzzification method:\n1-Wtaver\n2-Wtsum\n', [1,2]);
 end
 
-perturbation = input_function('Select the perturbation:\n1-No perturbation\n2-Actuator\n3-Charge\n4-Actuator and Charge\n', [1,2,3,4]);
+perturbation = input_function('Select the perturbation:\n1-No perturbation\n2-Actuator\n3-Charge\n', [1,2,3]);
 
 % load the pretended model and controller
 model_name = 'Models/model_';
@@ -41,11 +40,7 @@ else
     controller_name = strcat(controller_name, '25_');
 end
 
-if membership_function == 1
-    controller_name = strcat(controller_name, 'gaussmf.fis');
-else
-    controller_name = strcat(controller_name, 'trimf.fis');
-end
+controller_name = strcat(controller_name, 'gaussmf.fis');
 
 if reference_function == 1
 	model_name = strcat(model_name, 'square');
@@ -59,8 +54,6 @@ elseif perturbation == 2
 	model_name = strcat(model_name, '_actuator.slx');
 elseif perturbation == 3
 	model_name = strcat(model_name, '_charge.slx');
-elseif perturbation == 4
-    model_name = strcat(model_name, '_actuatorandcharge.slx');
 end
 
 controller = readfis(controller_name);
