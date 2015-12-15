c3a6f4390d54c844049017a366f37c51948db6d9
metalhead_6666 <noe.godinho.92@gmail.com>
2015-12-15 15:45:10 +0000

    run part B

---
+++ Assignment 4/runB.m
@@ -0,0 +1,23 @@
+% script to execute the part B of the assignment
+clear all;clc
+
+if exist('uy.mat', 'file') == 2
+    load('uy.mat');
+else
+    uiopen('ModelsB/newModel.slx', 1);
+    return;
+end
+
+valor = 4;
+[u_size, ~] = size(u);
+[y_size, ~] = size(y);
+matrix = zeros(u_size - valor + 1, 7);
+
+for i = 1 : 3
+    matrix(:,i) = y(valor - i:y_size - i);
+    matrix(:,i+3) = u(valor - i:u_size - i);
+end
+
+matrix(:,7) = y(valor:y_size);
+
+save('matrix.dat', 'matrix', '-ascii');
